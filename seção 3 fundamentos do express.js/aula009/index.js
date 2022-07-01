af5d19c505039d483bf4a3fc0fb9485d86632634
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-07-01 18:19:48 -0300

    parametros

--- seção 3 fundamentos do express.js/aula009/index.js
+++ seção 3 fundamentos do express.js/aula009/index.js
@@ -5,12 +5,24 @@ app.get('/', function(req, res) {
   res.send('<h1>Bem-vindo ao meu site!</h1>');
 });
 
-app.get('/blog', function(req, res) {
-  res.send('<h3>Bem-vindo ao meu blog!</h3>');
+app.get('/blog/:artigo?', function(req, res) {
+  let artigo = req.params.artigo;
+
+  if(artigo) {
+    res.send(`<h2>Artigo: ${artigo}</h2>`);
+  }else{
+    res.send('<h3>Bem-vindo ao meu blog!</h3>');
+  }
 });
 
 app.get('/canal/youtube', function(req, res) {
   res.send('<h1>Bem-vindo ao meu canal!</h1>');
+});
+
+app.get('/ola/:nome/:empresa', function(req, res) {
+  let nome = req.params.nome;
+  let empresa = req.params.empresa;
+  res.send(`<h1>Olá, ${nome} da empresa ${empresa}</h1>`);
 });
 
 app.listen(4000, function(erro) {
