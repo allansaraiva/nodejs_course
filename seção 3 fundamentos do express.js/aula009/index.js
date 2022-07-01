556ccf95a4c3547d45806bf7bdb834ba4e592845
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-07-01 17:31:36 -0300

    rotas

--- seção 3 fundamentos do express.js/aula009/index.js
+++ seção 3 fundamentos do express.js/aula009/index.js
@@ -1,6 +1,18 @@
 const express = require('express');  // importando o express
 const app = express();  // iniciando o express
 
+app.get('/', function(req, res) {
+  res.send('Bem-vindo ao meu site!');
+});
+
+app.get('/blog', function(req, res) {
+  res.send('Bem-vindo ao meu blog!');
+});
+
+app.get('/canal/youtube', function(req, res) {
+  res.send('Bem-vindo ao meu canal!');
+});
+
 app.listen(4000, function(erro) {
   if(erro){
     console.log('Ocorreu algum erro!');
