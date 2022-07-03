cbbc019c507265ac2b578df550d94d6fd4383386
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-07-03 17:28:05 -0300

    if else ejs

--- primeiroprojeto/index.js
+++ primeiroprojeto/index.js
@@ -6,11 +6,13 @@ app.set('view engine', 'ejs');
 app.get('/:nome/:lang', (req, res) => {
   let nome = req.params.nome;
   let lang = req.params.lang;
+  let exibirMensagem = false;
   res.render('index', {
     nome: nome,
     lang: lang,
     empresa: 'Guia do programador',
-    inscritos: 8000
+    inscritos: 8000,
+    mensagem: exibirMensagem
   });
 });
 
