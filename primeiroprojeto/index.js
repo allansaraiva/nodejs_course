e920f50c471b4693ce0430f299f40fbc690372a8
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-07-03 17:13:37 -0300

    variaveis no html

--- primeiroprojeto/index.js
+++ primeiroprojeto/index.js
@@ -3,8 +3,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('principal/perfil');
+app.get('/:nome/:lang', (req, res) => {
+  let nome = req.params.nome;
+  let lang = req.params.lang;
+  res.render('index', {
+    nome: nome,
+    lang: lang,
+    empresa: 'Guia do programador',
+    inscritos: 8000
+  });
 });
 
 app.listen(8080, () => {
