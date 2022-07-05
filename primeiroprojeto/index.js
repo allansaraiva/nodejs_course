33302e6bb727c8b1296013e42a510d48209b7ff2
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-07-04 22:08:30 -0300

    bootstrap

--- primeiroprojeto/index.js
+++ primeiroprojeto/index.js
@@ -4,27 +4,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:nome/:lang', (req, res) => {
-  let nome = req.params.nome;
-  let lang = req.params.lang;
-  let exibirMensagem = false;
-  let produtos = [
-    {nome: 'Doritos', preco: 3.14},
-    {nome: 'coca-cola', preco: 5},
-    {nome: 'Leite', preco: 1.45},
-    {nome: 'Carne', preco: 15},
-    {nome: 'Redbull', preco: 6},
-    {nome: 'Nescal', preco: 4}
-  ]
-
-  res.render('index', {
-    nome: nome,
-    lang: lang,
-    empresa: 'Guia do programador',
-    inscritos: 8000,
-    mensagem: exibirMensagem,
-    produtos:produtos
-  });
+app.get('/', (req, res) => {
+  
+  res.render('index');
 });
 
 app.listen(8080, () => {console.log('App rodando.');});
