2edc9944881ffc653c8ea9939824b27367d17c87
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-07-06 18:04:33 -0300

    enviando formulario para o node

--- primeiroprojeto/index.js
+++ primeiroprojeto/index.js
@@ -6,12 +6,14 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index');
-
 });
 
 app.get('/perguntar', (req, res) => {
   res.render('perguntar');
-  
+});
+
+app.post('/salvarpergunta', (req, res) => {
+  res.send('Formulário recebido!');
 });
 
 app.listen(8080, () => {console.log('App rodando.');});
