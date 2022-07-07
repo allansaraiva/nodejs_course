06189e18bc83c4792f2ae13798103dcdcb3511c0
Allan Saraiva <allan.saraiva2001@gmail.com>
2022-07-07 18:10:20 -0300

    buscando perguntas no banco de dados

--- primeiroprojeto/index.js
+++ primeiroprojeto/index.js
@@ -19,7 +19,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.render('index');
+  pergunta.findAll({raw: true}).then(perguntas => {
+    res.render('index', {
+      perguntas: perguntas,
+      
+    });
+  });
+  
 });
 
 app.get('/perguntar', (req, res) => {
